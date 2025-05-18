64316252ca5f88367037897ab6ba2ad2e65fcbd8
Bhupesh <bhupeshvyas202@gmail.com>
2025-05-18 19:54:17 +0530

    Created seo pages

---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,9 @@
+function AboutPage() {
+  return (
+    <div className="px-4">
+      <h1>AboutPage</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/app/(home)/pricing/page.tsx
@@ -0,0 +1,9 @@
+function PricingPage() {
+  return (
+    <div className="px-4">
+      <h1>PricingPage</h1>
+    </div>
+  );
+}
+
+export default PricingPage;
