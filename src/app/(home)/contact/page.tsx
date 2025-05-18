d3061eb95552f51046824fa3653cc7c95ce8dfbc
Bhupesh <bhupeshvyas202@gmail.com>
2025-05-18 19:54:36 +0530

    Created static seo pages

---
+++ src/app/(home)/contact/page.tsx
@@ -0,0 +1,9 @@
+function ContactPage() {
+  return (
+    <div className="px-4">
+      <h1>ContactPage</h1>
+    </div>
+  );
+}
+
+export default ContactPage;
---
+++ src/app/(home)/features/page.tsx
@@ -0,0 +1,9 @@
+function FeaturesPage() {
+  return (
+    <div className="px-4">
+      <h1>FeaturesPage</h1>
+    </div>
+  );
+}
+
+export default FeaturesPage;
