0a4a16f84b3a7407b4283393f91f597866aceffa
Bhupesh <bhupeshvyas202@gmail.com>
2025-07-06 01:35:52 +0530

    Updated user collection and created auth procedure and created procedure for session and register

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { authRouter } from "@/features/auth/server/procedure";
 import { categoriesRouter } from "@/features/categories/server/procedure";
 
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
+  auth: authRouter,
   categories: categoriesRouter,
 });
 
