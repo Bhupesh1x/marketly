48a477acea658bf31af5bf08b634943bebb541af
Bhupesh <bhupeshvyas202@gmail.com>
2025-07-12 18:32:54 +0530

    Added pagination for the products and No products found element

--- src/features/products/components/ProductList.tsx
+++ src/features/products/components/ProductList.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { InboxIcon } from "lucide-react";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+
+import { Button } from "@/components/ui/button";
 
 import { ProductCard } from "./ProductCard";
 
@@ -15,28 +20,66 @@ interface Props {
 export function ProductList({ category }: Props) {
   const trpc = useTRPC();
   const [filters] = useProductFilters();
-  const { data: products } = useSuspenseQuery(
-    trpc.products.getMany.queryOptions({
-      category,
-      ...filters,
-    })
+  const {
+    data: products,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useSuspenseInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions(
+      {
+        category,
+        ...filters,
+        limit: DEFAULT_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => {
+          return lastPage?.docs?.length > 0 ? lastPage.nextPage : undefined;
+        },
+      }
+    )
   );
 
+  if (!products?.pages?.[0]?.docs?.length) {
+    return (
+      <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-md">
+        <InboxIcon />
+        <p className="text-base font-medium">No products found</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-      {products?.docs?.map((product) => (
-        <ProductCard
-          key={product.id}
-          id={product.id}
-          name={product?.name || ""}
-          authorName="john"
-          authorImageUrl={null}
-          price={product.price}
-          rating={3}
-          reviewCount={5}
-          imageUrl={product?.image?.url || ""}
-        />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {products?.pages
+          ?.flatMap((page) => page?.docs ?? [])
+          ?.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product?.name || ""}
+              authorName="john"
+              authorImageUrl={null}
+              price={product.price}
+              rating={3}
+              reviewCount={5}
+              imageUrl={product?.image?.url || ""}
+            />
+          ))}
+      </div>
+      <div className="pt-8 flex justify-center">
+        {hasNextPage && (
+          <Button
+            variant="elevated"
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+            className="font-medium disabled:opacity-50 text-base bg-white"
+          >
+            Load more
+          </Button>
+        )}
+      </div>
+    </>
   );
 }
