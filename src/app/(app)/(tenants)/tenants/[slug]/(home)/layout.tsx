2b63890d8949bb7507903bace9735ea6e1d166b0
Bhupesh <bhupeshvyas202@gmail.com>
2025-07-13 20:42:56 +0530

    Integrated tenant getOne procedure with frontend and shown tenant name and image in the navbar

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,14 +1,33 @@
+import { Suspense } from "react";
+import { HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
 import { Footer } from "@/features/tenants/components/Footer";
-import { Navbar } from "@/features/tenants/components/Navbar";
+import { Navbar, NavbarSkeleton } from "@/features/tenants/components/Navbar";
 
 interface Props {
   children: React.ReactNode;
+  params: Promise<{ slug: string }>;
 }
 
-function TenantHomeLayout({ children }: Props) {
+async function TenantHomeLayout({ children, params }: Props) {
+  const { slug } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.tenants.getOne.queryOptions({
+      slug,
+    })
+  );
+
   return (
     <div className="min-h-screen w-full bg-[#f4f4f0]">
-      <Navbar />
+      <HydrationBoundary>
+        <Suspense fallback={<NavbarSkeleton />}>
+          <Navbar slug={slug} />
+        </Suspense>
+      </HydrationBoundary>
       <main className="w-full h-full">
         <div className="max-w-(--breakpoint-xl) mx-auto">{children}</div>
       </main>
--- src/features/tenants/components/Navbar.tsx
+++ src/features/tenants/components/Navbar.tsx
@@ -1,8 +1,45 @@
-export function Navbar() {
+"use client";
+
+import Image from "next/image";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
+interface Props {
+  slug: string;
+}
+
+export function Navbar({ slug }: Props) {
+  const trpc = useTRPC();
+  const { data: tenant } = useSuspenseQuery(
+    trpc.tenants.getOne.queryOptions({
+      slug,
+    })
+  );
+
   return (
     <nav className="h-20 border-b bg-white">
-      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full px-4 lg:px-12">
-        <h1 className="text-xl font-medium">Tenant</h1>
+      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center gap-2 h-full px-4 lg:px-12">
+        {!!tenant?.image?.url && (
+          <Image
+            src={tenant?.image?.url}
+            alt="tenant image"
+            height={32}
+            width={32}
+            className="rounded-full object-cover size-[32px]"
+          />
+        )}
+        <h1 className="text-xl font-medium">{tenant?.name || ""}</h1>
+      </div>
+    </nav>
+  );
+}
+
+export function NavbarSkeleton() {
+  return (
+    <nav className="h-20 border-b bg-white">
+      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center gap-2 h-full px-4 lg:px-12">
+        <div />
       </div>
     </nav>
   );
